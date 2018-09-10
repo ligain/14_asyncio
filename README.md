1f3abb40776d487c69ff2e71526755664dfe6f34
ligain <khokhlov86@gmail.com>
2018-09-10 22:22:47 +0300

    extended README refactored function to fetch urls

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# 14_asyncio
+# Ycrawler
+Yet another simple parser to parse and save news from [Ycombinator](https://news.ycombinator.com) to disk.
+It has following options:
+| Short option | Long option | Description |
+|:---:|:---:|:---:|
+| -o | --output-dir | dir where to put parsed data. [default `cwd`] |
+| -i | --interval | interval to check the main page in seconds [default 30] |
+| -l | --log-dir | dir for logs [default ./ycrawler.log] |
+
+### Run
+You should have installed **Python 3.6** on your system.
+```
+$ git clone https://github.com/ligain/14_asyncio
+$ cd 14_asyncio/
+$ python3.6 -m venv .env
+$ . .env/bin/activate
+$ pip install -r requirements.txt
+$ python crawler.py
+[2018.09.10 22:06:22] I Start crawling from main page: https://news.ycombinator.com
+...
+```
+### Tests
+```
+$ cd 14_asyncio/
+$ python3.6 tests.py
+```
+
+### Project Goals
+The code is written for educational purposes.
--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ import logging
 import os
 import re
 
-import aiohttp
+from aiohttp import ClientSession
 import asyncio
 
 from bs4 import BeautifulSoup, Tag
@@ -12,13 +12,27 @@ MAIN_PAGE = 'https://news.ycombinator.com'
 MAX_SLUG_LEN = 50
 
 
-async def get_url_content(session, url):
-    async with session.get(url) as response:
-        text_content = await response.text()
-    return BeautifulSoup(text_content, 'html.parser')
+async def get_url_content(session: ClientSession, url: str, retries=5):
+    while retries > 0:
+        try:
+            response = await session.get(url)
+        except:
+            logging.exception(f'An error while getting: {url}. Retrying...')
+            retries -= 1
+            continue
+        else:
+            if response.status == 200 and ('text/html' in response.headers.get('content-type')):
+                text_content = (await response.read()).decode('utf-8', 'replace')
+            else:
+                text_content = ''
+            response.close()
+            return BeautifulSoup(text_content, 'html.parser')
 
 
 def save_page(filename, dir, content: BeautifulSoup):
+    if not content.get_text():
+        logging.error(f'Empty content for file: {filename} in dir: {dir}')
+        return
     fullpath = os.path.join(dir, filename)
     with open(fullpath, 'w') as file:
         file.write(str(content))
@@ -46,12 +60,6 @@ def get_args():
         description='News Ycombinator parser'
     )
     parser.add_argument(
-        '-t', '--threads',
-        help='number of crawler threads [default 5]',
-        type=int,
-        default=5
-    )
-    parser.add_argument(
         '-o', '--output-dir',
         help='where to put parsed data [default .]',
         default=os.getcwd(),
@@ -60,7 +68,7 @@ def get_args():
     parser.add_argument(
         '-i', '--interval',
         help='interval to check the main page in seconds [default 30]',
-        default=10,
+        default=30,
         type=int
     )
     parser.add_argument(
@@ -72,20 +80,57 @@ def get_args():
     return parser.parse_args()
 
 
+async def process_comment_url(session: ClientSession, url: Tag, output_dir):
+    content = await get_url_content(session, url.get('href'))
+    logging.info(f"Comment content: {content.get_text()[:100]}")
+    comment_url_filename = slugify_url(url, use_href=True)
+    logging.info(f"Save comment url in file: {comment_url_filename} in folder: {output_dir}")
+    save_page(comment_url_filename, output_dir, content)
+
+
+async def process_news_url(session: ClientSession, url: Tag, output_dir):
+    if not url.get('href').startswith('http'):
+        comment_page_full_url = os.path.join(MAIN_PAGE, url.get('href'))
+    else:
+        comment_page_full_url = url.get('href')
+    logging.info(f"Parsing content of page: {comment_page_full_url}")
+    comment_page_content = await get_url_content(session, comment_page_full_url)
+
+    news_url = comment_page_content.select('td.title > a')[0]
+    news_content = await get_url_content(session, news_url.get('href'))
+    news_folder_name = slugify_url(news_url)
+    news_full_path = os.path.join(output_dir, news_folder_name)
+    if not os.path.exists(news_full_path):
+        logging.info(f"Create new folder: {news_folder_name} for a piece of news")
+        os.mkdir(news_full_path)
+
+        # save article page
+        logging.info(f"Save article.html in folder: {news_folder_name}")
+        save_page('article.html', news_full_path, news_content)
+
+        # save comments page
+        logging.info(f"Save comments of page: {comment_page_full_url} in file: comments.html")
+        save_page('comments.html', news_full_path, comment_page_content)
+
+        # save comments urls
+        comments_urls = comment_page_content.select('div.comment a[rel="nofollow"]')
+        for comment_url in comments_urls:
+            await process_comment_url(session, comment_url, news_full_path)
+    else:
+        logging.info(f"News page: {news_url.get('href')} already exists in output dir. Skipping...")
+
+
 async def main(url, args):
     logging.info(f'Start crawling from main page: {url}')
-    main_page_queue = asyncio.Queue()
 
-    # while True:
-    async with aiohttp.ClientSession(conn_timeout=60) as session:
-        main_page = await get_url_content(session, url)
-        story_links = main_page.find_all('a', class_='storylink')
-        print(story_links)
-        save_page('sdf.html', '.', main_page)
-        await main_page_queue.put(story_links[0])
-        print(f"main_page_queue: size: {main_page_queue.qsize()}")
-            # await asyncio.sleep(args.interval)
-    logging.info('Crawling is finished')
+    while True:
+        async with ClientSession(conn_timeout=60) as session:
+            main_page = await get_url_content(session, url)
+            comments_links = main_page.select('td.subtext > a:nth-of-type(3)')
+            for comment_link in comments_links:
+                await process_news_url(session, comment_link, args.output_dir)
+        logging.info('Crawling is finished')
+        await asyncio.sleep(args.interval)
 
 
 if __name__ == '__main__':
